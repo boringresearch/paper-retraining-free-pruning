0c8029177a120be853cbc9bf8fc98aec209eef66
xihajun <jhuang10@ed.ac.uk>
2023-04-03 11:19:58 +0100

    Skip pruning if exists

--- main.py
+++ main.py
@@ -145,83 +145,93 @@ def main():
 
     full_head_mask = torch.ones(config.num_hidden_layers, config.num_attention_heads).cuda()
     full_neuron_mask = torch.ones(config.num_hidden_layers, config.intermediate_size).cuda()
+    
+    head_mask_path = os.path.join(args.output_dir, "head_mask.pt")
+    neuron_mask_path = os.path.join(args.output_dir, "neuron_mask.pt")
+    
+    if not os.path.exists(head_mask_path) or not os.path.exists(neuron_mask_path):
 
-    start = time.time()
-    # Search the optimal mask
-    head_grads, neuron_grads = collect_mask_grads(
-        model,
-        full_head_mask,
-        full_neuron_mask,
-        sample_dataloader,
-    )
-    teacher_constraint = get_pruning_schedule(target=args.constraint, num_iter=2)[0]
-    if args.metric == "mac":
-        teacher_head_mask, teacher_neuron_mask = search_mac(
-            config,
-            head_grads,
-            neuron_grads,
-            seq_len,
-            teacher_constraint,
+        start = time.time()
+        # Search the optimal mask
+        head_grads, neuron_grads = collect_mask_grads(
+            model,
+            full_head_mask,
+            full_neuron_mask,
+            sample_dataloader,
         )
-        head_mask, neuron_mask = search_mac(
-            config,
-            head_grads,
-            neuron_grads,
-            seq_len,
-            args.constraint,
-        )
-        pruned_mac, orig_mac = compute_mask_mac(head_mask, neuron_mask, seq_len, config.hidden_size)
-        logger.info(f"Pruned Model MAC: {pruned_mac / orig_mac * 100.0:.2f} %")
-    elif args.metric == "latency":
-        mha_lut = torch.load(args.mha_lut)
-        ffn_lut = torch.load(args.ffn_lut)
-        teacher_head_mask, teacher_neuron_mask = search_latency(
-            config,
-            head_grads,
-            neuron_grads,
-            teacher_constraint,
-            mha_lut,
-            ffn_lut,
-        )
-        head_mask, neuron_mask = search_latency(
-            config,
-            head_grads,
-            neuron_grads,
-            args.constraint,
-            mha_lut,
-            ffn_lut,
-        )
-        pruned_latency = estimate_latency(mha_lut, ffn_lut, head_mask, neuron_mask)
-        logger.info(f"Pruned Model Latency: {pruned_latency:.2f} ms")
+        teacher_constraint = get_pruning_schedule(target=args.constraint, num_iter=2)[0]
+        if args.metric == "mac":
+            teacher_head_mask, teacher_neuron_mask = search_mac(
+                config,
+                head_grads,
+                neuron_grads,
+                seq_len,
+                teacher_constraint,
+            )
+            head_mask, neuron_mask = search_mac(
+                config,
+                head_grads,
+                neuron_grads,
+                seq_len,
+                args.constraint,
+            )
+            pruned_mac, orig_mac = compute_mask_mac(head_mask, neuron_mask, seq_len, config.hidden_size)
+            logger.info(f"Pruned Model MAC: {pruned_mac / orig_mac * 100.0:.2f} %")
+        elif args.metric == "latency":
+            mha_lut = torch.load(args.mha_lut)
+            ffn_lut = torch.load(args.ffn_lut)
+            teacher_head_mask, teacher_neuron_mask = search_latency(
+                config,
+                head_grads,
+                neuron_grads,
+                teacher_constraint,
+                mha_lut,
+                ffn_lut,
+            )
+            head_mask, neuron_mask = search_latency(
+                config,
+                head_grads,
+                neuron_grads,
+                args.constraint,
+                mha_lut,
+                ffn_lut,
+            )
+            pruned_latency = estimate_latency(mha_lut, ffn_lut, head_mask, neuron_mask)
+            logger.info(f"Pruned Model Latency: {pruned_latency:.2f} ms")
 
-    # Rearrange the mask
-    head_mask = rearrange_mask(head_mask, head_grads)
-    neuron_mask = rearrange_mask(neuron_mask, neuron_grads)
+        # Rearrange the mask
+        head_mask = rearrange_mask(head_mask, head_grads)
+        neuron_mask = rearrange_mask(neuron_mask, neuron_grads)
 
-    # Rescale the mask by solving a least squares problem
-    head_mask, neuron_mask = rescale_mask(
-        model,
-        config,
-        teacher_head_mask,
-        teacher_neuron_mask,
-        head_mask,
-        neuron_mask,
-        sample_dataloader,
-        classification_task=not IS_SQUAD,
-    )
+        # Rescale the mask by solving a least squares problem
+        head_mask, neuron_mask = rescale_mask(
+            model,
+            config,
+            teacher_head_mask,
+            teacher_neuron_mask,
+            head_mask,
+            neuron_mask,
+            sample_dataloader,
+            classification_task=not IS_SQUAD,
+        )
 
-    # Print the pruning time
-    end = time.time()
-    logger.info(f"{args.task_name} Pruning time (s): {end - start}")
+        # Print the pruning time
+        end = time.time()
+        logger.info(f"{args.task_name} Pruning time (s): {end - start}")
+        issaving = True
+    else:
+        head_mask = torch.load(head_mask_path)
+        neuron_mask = torch.load(neuron_mask_path)
+        issaving = False
 
     # Evaluate the accuracy
     test_acc = test_accuracy(model, head_mask, neuron_mask, tokenizer, args.task_name)
     logger.info(f"{args.task_name} Test accuracy: {test_acc:.4f}")
-
+    
     # Save the masks
-    torch.save(head_mask, os.path.join(args.output_dir, "head_mask.pt"))
-    torch.save(neuron_mask, os.path.join(args.output_dir, "neuron_mask.pt"))
-
+    if issaving:
+        torch.save(head_mask, os.path.join(args.output_dir, "head_mask.pt"))
+        torch.save(neuron_mask, os.path.join(args.output_dir, "neuron_mask.pt"))
 
 if __name__ == "__main__":
     main()
